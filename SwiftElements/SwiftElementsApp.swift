27d38412aa46ba286af4dcd544c338e2db9838c4
RyotaFukumoto <k014c1106@it-neec.com>
2024-10-05 21:58:13 +0900

    Initial Commit

---
+++ SwiftElements/SwiftElementsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftElementsApp.swift
+//  SwiftElements
+//
+//  Created by Ryota F on 2024/10/05.
+//
+
+import SwiftUI
+
+@main
+struct SwiftElementsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
